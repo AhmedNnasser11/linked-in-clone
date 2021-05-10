3fe2046326d7fe43c2a82ffb51dffeca2d205171
ahmednnasser11@gmail.com <ahmednnasser11@gmail.com>
2021-05-10 20:14:39 +0200

    add some code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>linked in</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
