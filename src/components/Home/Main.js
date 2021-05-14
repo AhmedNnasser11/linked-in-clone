21bdbb5fc302edc94867e8683bfcef264efdb3ad
ahmednnasser11@gmail.com <ahmednnasser11@gmail.com>
2021-05-14 03:29:37 +0200

    end left section

---
+++ src/components/Home/Main.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const Main = () => {
+    return (
+        <Container>
+            main
+        </Container>
+    )
+}
+
+const Container = styled.section`
+    grid-area: main;
+`;
+
+export default Main
---
+++ src/components/Home/RightSide.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const RightSide = () => {
+    return (
+        <Container>
+            RightSide
+        </Container>
+    )
+}
+
+const Container = styled.section`
+    grid-area: rightside;
+`;
+
+export default RightSide
